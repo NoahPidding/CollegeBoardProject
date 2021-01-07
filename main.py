de991e7ac0bbf305ce5e9c33d78f805bb2916781
NiharMarar <mararnihar@gmail.com>
2021-01-07 14:05:52 -0800

    Pig Latin function into post

--- main.py
+++ main.py
@@ -207,19 +207,32 @@ def PigLatin():
 def encryptionPL():
     if request.method == 'POST':
         form = request.form
-        PL_text = form["PL1"]
-        result1 = ''.join(format(ord(i), 'b') for i in PL_text)
-        return render_template("PigLatin.html", display = result1)
-    return redirect("/PigLatin")
-
-#runs Pig Latin Cipher decryption
-@app.route("/PL_decrypt", methods=['GET','POST'])
-def decryptionPL():
-    if request.method == 'POST':
-        form = request.form
-        PL_text = form["PL1"]
-        string = ' '
-        return render_template("PigLatin.html", display = string)
+        #!/usr/bin/env python
+        # This program translates a string to Pig Latin
+        sentence = form["PL1"]
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        # Split sentence into a list of words
+        words = sentence.split()
+        for i, word in enumerate(words):
+            # Split each word into a list of letters
+            letters = list(word)
+            first_vowel = 0
+            # Loop through letters of the word to find the first vowel
+            for j, letter in enumerate(letters):
+                if letter.lower() in vowels:
+                    first_vowel = j
+                    break
+            # Add 'yay' if the word starts with a vowel
+            if first_vowel == 0:
+                word = word + "yay"
+            # Otherwise, move the beginning consonants + 'ay' to the end of the word
+            else:
+                word = word[first_vowel:len(word)] + word[0:first_vowel] + "ay"
+            # Replace the English word with the Pig Latin word in the list
+            words[i] = word.lower()
+        # Reconstructs a new Pig Latin sentence from the list of words
+        sentence = " ".join(words)
+        return render_template("PigLatin.html", display = sentence)
     return redirect("/PigLatin")
 
 #Pig Latin info
@@ -241,30 +254,3 @@ def QuizGame():
 if __name__ == "__main__":
     app.run(debug = True)
 
-#!/usr/bin/env python
-# This program translates a string to Pig Latin
-
-sentence = input("Type in a sentence to translate to Pig Latin: ")
-vowels = ['a', 'e', 'i', 'o', 'u']
-# Split sentence into a list of words
-words = sentence.split()
-for i, word in enumerate(words):
-    # Split each word into a list of letters
-    letters = list(word)
-    first_vowel = 0
-    # Loop through letters of the word to find the first vowel
-    for j, letter in enumerate(letters):
-        if letter.lower() in vowels:
-            first_vowel = j
-            break
-    # Add 'yay' if the word starts with a vowel
-    if first_vowel == 0:
-        word = word + "yay"
-    # Otherwise, move the beginning consonants + 'ay' to the end of the word
-    else:
-        word = word[first_vowel:len(word)] + word[0:first_vowel] + "ay"
-    # Replace the English word with the Pig Latin word in the list
-    words[i] = word.lower()
-# Reconstructs a new Pig Latin sentence from the list of words
-sentence = " ".join(words)
-print(sentence)
