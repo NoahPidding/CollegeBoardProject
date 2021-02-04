8636f8b40edde0285d197e2a7610b39325308073
NoahPidding <noah.pidding@gmail.com>
2021-02-04 14:02:19 -0800

    api pig latin

--- main.py
+++ main.py
@@ -330,7 +330,30 @@ def api():
     response = requests.request("GET", url, headers=headers)
     quote = response.json().get('content')
     """print(response.text)"""
-    return render_template("api.html", quote=quote)
+    sentencepl = quote
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    # Split sentence into a list of words
+    words = sentencepl.split()
+    for i, word in enumerate(words):
+        # Split each word into a list of letters
+        letters = list(word)
+        first_vowel = 0
+        # Loop through letters of the word to find the first vowel
+        for j, letter in enumerate(letters):
+            if letter.lower() in vowels:
+                first_vowel = j
+                break
+        # Add 'yay' if the word starts with a vowel
+        if first_vowel == 0:
+            word = word + "yay"
+        # Otherwise, move the beginning consonants + 'ay' to the end of the word
+        else:
+            word = word[first_vowel:len(word)] + word[0:first_vowel] + "ay"
+        # Replace the English word with the Pig Latin word in the list
+        words[i] = word.lower()
+    # Reconstructs a new Pig Latin sentence from the list of words
+    sentencepl = " ".join(words)
+    return render_template("api.html", quote=quote, encryptPL=sentencepl)
 
 
 #run file
