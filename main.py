8410c47299f1ba581184aea14a391258d6452502
NiharMarar <mararnihar@gmail.com>
2021-04-30 21:40:24 -0700

    class code

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import rsa
 import requests
 
 #create "Flask"
+from cipherclass import Cipher
+
 app = Flask(__name__)
 
 #links dictionary
@@ -320,97 +322,17 @@ def easteregg():
 
 @app.route('/api',  methods=['GET', 'POST'])
 def api():
+    # This is the setup for the rapid api
     url = "https://quotes15.p.rapidapi.com/quotes/random/"
-
     headers = {
         'x-rapidapi-key': "fbc28ca63amsh13b8750406531e6p1bf8a9jsnabe9c216b21b",
         'x-rapidapi-host': "quotes15.p.rapidapi.com"
     }
-
+    # This code retrieves a quote from rapid api
     response = requests.request("GET", url, headers=headers)
     quote = response.json().get('content')
-    """print(response.text)"""
-    sentencepl = quote
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    # Split sentence into a list of words
-    words = sentencepl.split()
-    for i, word in enumerate(words):
-        # Split each word into a list of letters
-        letters = list(word)
-        first_vowel = 0
-        # Loop through letters of the word to find the first vowel
-        for j, letter in enumerate(letters):
-            if letter.lower() in vowels:
-                first_vowel = j
-                break
-        # Add 'yay' if the word starts with a vowel
-        if first_vowel == 0:
-            word = word + "yay"
-        # Otherwise, move the beginning consonants + 'ay' to the end of the word
-        else:
-            word = word[first_vowel:len(word)] + word[0:first_vowel] + "ay"
-        # Replace the English word with the Pig Latin word in the list
-        words[i] = word.lower()
-    # Reconstructs a new Pig Latin sentence from the list of words
-    sentencepl = " ".join(words)
-
-    B_text = quote
-    result = ''.join(format(ord(i), 'b') for i in B_text)
-
-    code = (".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..")
-    # tuple containing the Morse code for digits
-    digit = ("-----",".----","...--","....-",".....","-....","--...","---..","----.")
-    morse = ""
-    # Morse code has no cases, so work in upper case
-    text = quote.upper()
-    # look through each character in the input
-    for char in text:
-        # if it's a letter
-        if char >= "A" and char <= "Z":
-            # use ASCII code to calculate as index for code
-            # A=65, B=66... so subtracting 65 gives 0, 1...
-            morse += (code[ord(char)-65] + " ")
-        # if it's a digit
-        if char >= "0" and char <= "9":
-            # use the integer version as index for digit
-            morse += (digit[eval(char)] + " ")
-            # if it's a space
-        if char == " ":
-            morse += " / "
-
-    text1 = quote
-    s = 1
-    def encryptionCC1(text1,s):
-        result = []
-        # transverse the plain text
-
-        # for letter in text1:
-        for i in range(0,len(text1)):
-            char = text1[i]
-            # Encrypt uppercase characters in plain text
-
-            if (char.isupper()):
-                L = chr((ord(char) + s-65) % 26 + 65)
-            # Encrypt lowercase characters in plain text
-            elif (char.islower()):
-                L = chr((ord(char) + s - 97) % 26 + 97)
-            else:
-                L = chr(ord(char))
-            result.append(L)
-        return result
-    result1=encryptionCC1(text1,s)
-    sentencecc="".join(result1)
-
-    message = quote
-    keys = rsa.keyGen()
-    pubKey1 = 9
-    pubKey2 = 169133477
-    #get encrypted and make into useable output
-    encrypted = rsa.rsa(message, pubKey1, pubKey2)
-    encrypted = encrypted[0]
-    encrypted = ''.join(encrypted)
-    # Private Key = 150317579
-    return render_template("api.html", quote=quote, encryptPL=sentencepl, binary=result, encryptMC=morse, encryptCC=sentencecc, encryptRSA=encrypted)
+    # This calls the cipher class with the quote and returns the object(cipher) to the html page
+    return render_template("api.html", cipher=Cipher(quote))
 
 #account login
 @app.route('/security')
